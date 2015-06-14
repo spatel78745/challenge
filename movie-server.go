6e29e59b14ac6493f97e98246ec709409dc52bdc
Sameer Patel <sameer@m-87.com>
2015-06-14 18:12:00 -0500

    Copy files from tutorial directory

---
+++ movie-server.go
@@ -0,0 +1,118 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type comment struct {
+	Title  string `json:"title"`
+	Genre  string `json:"genre"`
+	Actors string `json:"actors"`
+	Year   string `json:"year"`
+	Rating string `json:"rating"`
+}
+
+const dataFile = "./movies.json"
+
+var commentMutex = new(sync.Mutex)
+
+// Handle comments
+func handleComments(w http.ResponseWriter, r *http.Request) {
+	// Since multiple requests could come in at once, ensure we have a lock
+	// around all file operations
+	commentMutex.Lock()
+	defer commentMutex.Unlock()
+
+	// Stat the file, so we can find its current permissions
+	fi, err := os.Stat(dataFile)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to stat the data file (%s): %s", dataFile, err), http.StatusInternalServerError)
+		return
+	}
+
+	// Read the comments from the file.
+	commentData, err := ioutil.ReadFile(dataFile)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to read the data file (%s): %s", dataFile, err), http.StatusInternalServerError)
+		return
+	}
+
+	switch r.Method {
+	case "POST":
+    log.Println("Got post");
+		// Decode the JSON data
+		comments := make([]comment, 0)
+		if err := json.Unmarshal(commentData, &comments); err != nil {
+			http.Error(w, fmt.Sprintf("Unable to Unmarshal comments from data file (%s): %s", dataFile, err), http.StatusInternalServerError)
+			return
+		}
+
+		// Add a new comment to the in memory slice of comments
+		comments = append(comments, comment{
+        Title: r.FormValue("title"),
+        Genre: r.FormValue("genre"),
+        Actors: r.FormValue("actors"),
+        Year: r.FormValue("year"),
+        Rating: r.FormValue("rating"),
+      })
+
+		// Marshal the comments to indented json.
+		commentData, err = json.MarshalIndent(comments, "", "    ")
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Unable to marshal comments to json: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		// Write out the comments to the file, preserving permissions
+		err := ioutil.WriteFile(dataFile, commentData, fi.Mode())
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Unable to write comments to data file (%s): %s", dataFile, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache")
+		io.Copy(w, bytes.NewReader(commentData))
+
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache")
+		// stream the contents of the file to the response
+		io.Copy(w, bytes.NewReader(commentData))
+
+	default:
+		// Don't know the method, so error
+		http.Error(w, fmt.Sprintf("Unsupported method: %s", r.Method), http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	http.HandleFunc("/movies.json", handleComments)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println("Server started: http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
---
+++ public/scripts/challenge.js
@@ -0,0 +1,228 @@
+var Table = React.createClass({
+    render: function render() {
+        var _self = this;
+
+        var thead = React.DOM.thead({},
+            React.DOM.tr({},
+                this.props.cols.map(function (col) {
+                    return React.DOM.th({}, col);
+            })));
+
+        var tbody = this.props.rows.map(function (row) {
+            return React.DOM.tr({},
+            _self.props.cols.map(function (col) {
+                return React.DOM.td({}, row[col] || "");
+            }));
+        });
+
+        return React.DOM.table(null, [thead, tbody]);
+    }
+
+});
+
+var MovieCollectionApp = React.createClass({
+    getInitialState: function() {
+      return {data: []};
+    },
+
+  loadMoviesFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+     });
+    },
+
+  componentDidMount: function() {
+    this.loadMoviesFromServer();
+    setInterval(this.loadMoviesFromServer, this.props.pollInterval);
+  },
+
+  handleMovieSubmit: function(movie) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: movie,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  render: function() {
+    return (
+      <div className="movieCollectionApp">
+        <h1>Movie Manager</h1>
+        <MovieForm onMovieSubmit={this.handleMovieSubmit} />
+        <hr />
+        <MovieTable data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var MovieTable = React.createClass({
+  getInitialState: function() {
+    return {pattern: ""};
+  },
+
+  handleMovieSearch: function (pattern) {
+    this.setState({pattern: pattern.toLowerCase()});
+  },
+
+  render: function() {
+    var movies=this.props.data;
+    var tableCols=["Title", "Genre", "Actors", "Year", "Rating"];
+
+    console.log("INITIAL STATE: " + this.state.pattern);
+
+    function compare(row1, row2) {
+      return row1.Title.localeCompare(row2.Title);
+    }
+
+    function filter(rows, pattern) {
+      console.log("filtering on: " + pattern);
+      if (!pattern) return rows;
+
+      for(i = 0, j = 0, filteredRows = []; i < rows.length; i++) {
+
+        console.log("Matching: " + rows[i].Title + " against " + pattern);
+
+        var result =
+          (rows[i].Title.toLowerCase().search(pattern)  != -1) ||
+          (rows[i].Genre.toLowerCase().search(pattern)  != -1) ||
+          (rows[i].Actors.toLowerCase().search(pattern) != -1) ||
+          (rows[i].Year.toLowerCase().search(pattern)   != -1) ||
+          (rows[i].Rating.toLowerCase().search(pattern) != -1);
+
+        console.log("Result: " + result);
+
+        if (result) {
+          console.log("index " + i + " with title " + rows[i].Title + " matches " + pattern);
+          filteredRows[j] = rows[i];
+          j++;
+        }
+      }
+
+      return filteredRows;
+    }
+
+    for(i = 0, tableRows = []; i < movies.length; i++) {
+      tableRows[i]={
+        Title  : movies[i].title,
+        Genre  : movies[i].genre,
+        Actors : movies[i].actors,
+        Year   : movies[i].year,
+        Rating : movies[i].rating,
+      }
+    }
+
+
+    tableRows.sort(compare);
+
+    tableRows = filter(tableRows, this.state.pattern);
+
+    return (
+      <div className="movieTable">
+        <MovieSearch onMovieSearch={this.handleMovieSearch} />
+        <br/>
+        <Table cols={tableCols} rows={tableRows}/>
+      </div>
+    );
+  }
+});
+
+
+
+var Movie = React.createClass({
+  render: function() {
+    return (
+      <div className="movie">
+        <h3 className="movieTitle">
+          {this.props.title}
+        </h3>
+        {this.props.genre}, {this.props.actors}, {this.props.year}, {this.props.rating}
+      </div>
+    );
+  }
+});
+
+var MovieForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = React.findDOMNode(this.refs.title).value.trim();
+    var genre = React.findDOMNode(this.refs.genre).value.trim();
+    var actors = React.findDOMNode(this.refs.actors).value.trim();
+    var year = React.findDOMNode(this.refs.year).value.trim();
+    var rating = React.findDOMNode(this.refs.rating).value.trim();
+
+    if (!title || !genre || !actors || !year || !rating) {
+      return;
+    }
+
+    this.props.onMovieSubmit({title: title, genre: genre, actors: actors, year: year, rating: rating});
+
+    React.findDOMNode(this.refs.title).value = '';
+    React.findDOMNode(this.refs.genre).value = '';
+    React.findDOMNode(this.refs.actors).value = '';
+    React.findDOMNode(this.refs.year).value = '';
+    React.findDOMNode(this.refs.rating).value = '';
+    return;
+  },
+
+  render: function() {
+    return (
+      <div className="newMovie">
+        <h3>Add New Movie</h3>
+        <form className="movieForm" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" ref="title" />
+          <input type="text" placeholder="Genre" ref="genre" />
+          <input type="text" placeholder="Actors" ref="actors"/>
+          <input type="text" placeholder="Year" ref="year" />
+          <input type="text" placeholder="Rating" ref="rating"/>
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
+});
+
+var MovieSearch = React.createClass({
+  handleSearch: function(e) {
+    e.preventDefault();
+
+    var pattern = React.findDOMNode(this.refs.pattern).value.trim();
+    console.log("pattern: " + pattern);
+
+    this.props.onMovieSearch(pattern);
+
+    return;
+  },
+
+  render: function() {
+    return (
+      <div className="newMovie">
+        <h3>Search</h3>
+        <h4>Enter a partial title, genre, actor, year or rating</h4>
+        <form className="movieSearch" onSubmit={this.handleSearch}>
+          <input type="search" incremental="incremental" onsearch={this.handleSearch} ref="pattern" />
+        </form>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <MovieCollectionApp url="movies.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
