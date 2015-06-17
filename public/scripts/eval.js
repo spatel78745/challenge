a1649500410575c2091ee6c75c1d4dced764c0e2
Sameer Patel <sameer@m-87.com>
2015-06-17 09:15:35 -0500

    - matching basically works

--- public/scripts/eval.js
+++ public/scripts/eval.js
@@ -35,13 +35,26 @@ var StrExp = function(op1) {
 
     return context.search(that.op1) != -1
   }
+
+  this.getOps = function() {
+    var that = this;
+
+    return that.op1;
+  }
+}
+
+var TrueExp = function() {
+  this.eval = function(context) {
+    return true;
+  }
 }
 
 var Tokenizer = function(str) {
+  this.str = str;
   this.lexemes = str.split(" ");
   this.index = 0;
 
-  this.getToken = function() {
+  this.peek = function() {
     var that = this;
 
     if (that.index >= that.lexemes.length) return { type: "EOF", val: "" };
@@ -49,13 +62,60 @@ var Tokenizer = function(str) {
     var lexeme = that.lexemes[that.index];
     var c = lexeme.charAt(0);
 
-    if      (c == '&') token = { type: "AND", val: ""     };
-    else if (c == '|') token = { type: "OR" , val: ""     };
-    else               token = { type: "STR", val: lexeme };
-
-    that.index++;
+    if      (c == '&') token = { type: "AND"   , val: "&"    } ;
+    else if (c == '|') token = { type: "OR"    , val: "|"    } ;
+    else if (c == '(') token = { type: "OPEN"  , val: "("    } ;
+    else if (c == ')') token = { type: "CLOSE" , val: ")"    } ;
+    else               token = { type: "STR"   , val: lexeme } ;
 
     return token;
+  }
+
+  this.get = function() {
+    var that = this;
+
+    token = that.peek();
+    if (token.type != "EOF") that.index++;
+
+    return token;
+  }
+
+  this.getStr = function() {
+    var that = this;
+
+    return that.str;
+  }
+}
+
+function makeMatcher(tokenizer)
+{
+  token = tokenizer.get();
+  console.log("token: " + token.type + " " + token.val);
+  if (token.type != "STR") {
+    console.log("Error: expected string, got [ " + token.val + " ]");
+    return new TrueExp;
+  }
+
+  subExp = new StrExp(token.val);
+  console.log("new StrExp: " + subExp.getOps());
+
+  lookahead = tokenizer.peek();
+  console.log("lookahead: " + lookahead.type + " " + lookahead.val);
+
+  if (lookahead.type == "EOF") 
+  {
+    console.log("No more tokens");
+    return subExp;
+  }
+
+  tokenizer.get();
+
+  if (lookahead.type == "STR" || lookahead.type == "AND" ) {
+    return new AndExp(subExp, makeMatcher(tokenizer));
+  }
+
+  if (lookahead.type == "OR") {
+    return new OrExp(subExp , makeMatcher(tokenizer));
   }
 }
 
@@ -78,11 +138,19 @@ console.log("s3 || s4: " +  o2.eval(context));
 var a2 = new AndExp(o1, o2);
 console.log("o1 && o2: " +  a2.eval(context));
 
-var pattern = "to & be & not";
+var pattern = "to & be | ass";
 var token = { type: "", val: "" };
-tokenizer = new Tokenizer(pattern);
 
-while(token.type != "EOF") {
-  token = tokenizer.getToken();
-  console.log("token: " + token.type + " val: " + token.val);
-}
+tokenizer = new Tokenizer(pattern);
+matcher = makeMatcher(tokenizer);
+
+console.log("Made matcher: " + matcher);
+
+console.log("Does it match? " + matcher.eval(context));
+
+//matcher.eval(context);
+
+//while(token.type != "EOF") {
+//  token = tokenizer.get();
+//  console.log("token: " + token.type + " val: " + token.val);
+//}
